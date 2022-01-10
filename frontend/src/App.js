7d644f9597f329193524cf66272a61ca1dd37b10
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-10 16:51:23 +0200

    Modify frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">SAS</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function get() {
+      let response = await fetch("roki-nodejs-test.herokuapp.com/", {
+        method: "GET",
+      });
+      let data = await response.json();
+      setData(data);
+    }
+
+    get();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>SAS</h1>
+
+      <ul>
+        {data.map((part) => (
+          <li key={part.id}>{`${part.latitude}, ${part.longitude}`}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
